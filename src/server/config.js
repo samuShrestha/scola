b176395763e51ea6528019042b53b6c6f33f63b5
Samu Shrestha <aweosme.jr.arch@gmail.com>
2018-04-27 03:07:56 -0600

    heroku deploy update

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "port": 8080,
+    "port": process.env.PORT || 8080,
     "dbUri": "mongodb://scola:eastonhasalilweewee@ds014648.mlab.com:14648/scola",
     "jwtSecret": "easton has a lil wee wee!!"
 };
